a0996259a336c3bbf481e3bc49a6239f61b6ee53
Eric Ober <ericscottober@gmail.com>
2021-03-11 17:00:32 -0500

    Updated app.js to import browserrouter, route, switch, components, pages, and return browserrouter with nav, figure, and footer inside and swtich with home and portfolio routes inside of that

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import Nav from './components/nav';
+import Home from './pages/home';
+import Portfolio from './pages/portfolio';
+import Figure from './components/figure';
+import Footer from './components/footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <Switch>
+        <Route component={Home} path='/' exact />
+        <Route component={Portfolio} path='/pages' />
+      </Switch>
+      <Figure />
+      <Footer />
+    </BrowserRouter>
   );
 }
 
